fc4413adc5cc1c6dfd878baf20ae82812958b009
Javier de Marco <javierdemarcoo@gmail.com>
2023-06-29 22:42:01 +0200

    Added Mason, Mason-lspconfig, nvim-lspconfig, mini.completion (luaSnip doesn't work)

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -11,7 +11,7 @@ return require('packer').startup(function(use)
   }
 
   -----------------------------------------------
-  ----------------- GIT   ----------------------
+  -----------------  GIT  ----------------------
   -----------------------------------------------
 
   -- Lazygit inside vim
@@ -24,6 +24,41 @@ return require('packer').startup(function(use)
 
   -- Fugitive
   use { 'tpope/vim-fugitive' }
+  
+  -----------------------------------------------
+  -----------------  LSP  ----------------------
+  -----------------------------------------------
+  -- LSP Installer Mason
+  use {
+    "williamboman/mason.nvim",
+    config = function()
+      require("mason").setup()
+    end
+  }
+  use {
+    "williamboman/mason-lspconfig.nvim",
+    config = function()
+      require("mason-lspconfig").setup()
+      require("mason-lspconfig").setup_handlers {
+        function (server_name)
+          require('lspconfig')[server_name].setup {}
+        end
+      }
+    end
+
+  }
+  use {
+    "neovim/nvim-lspconfig",
+  }
+use {'echasnovski/mini.completion',
+config = function()
+  require('mini.completion').setup()
+end
+}
+use{
+	"L3MON4D3/LuaSnip",
+}
+  --use 'ms-jpq/coq_nvim'
 
 
 end)
